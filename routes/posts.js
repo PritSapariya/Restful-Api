bf2deb7a2829d10ac357556e6c76ad50a73319e4
PritSapariya <pritsapariya17@gmail.com>
2021-04-11 15:59:18 +0530

    Model and Basic Router Done

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('we are on posts');
+//GETTING ALL POSTS
+router.get('/', async (req, res) => {
+    try {
+        const collectedPosts = await Post.find();
+        res.json(collectedPosts);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+//SUBMITING A POST
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 module.exports = router;
